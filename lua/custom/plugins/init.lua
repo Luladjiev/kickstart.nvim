881d86a7aa96369ca57dc7e8eeb86da01708e67c
Peter Luladjiev <git@luladjiev.com>
2024-01-20 17:48:04 +0200

    Add next / previous buffer keybinds

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.keymap.set('n', '<TAB>', ':bn<cr>', { desc = 'Next Buffer' })
+vim.keymap.set('n', '<S-TAB>', ':bp<cr>', { desc = 'Previous Buffer' })
+
 return {}
