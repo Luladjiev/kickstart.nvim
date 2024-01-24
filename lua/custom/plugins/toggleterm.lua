a9a2230bf27c0c1dab91e55d6da4497150414c98
Peter Luladjiev <git@luladjiev.com>
2024-01-24 18:54:05 +0200

    Fix detection of win32

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,26 +1,22 @@
 local function _lazygit_toggle()
-  local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new { cmd = "lazygit", direction = "float", hidden = true }
+  local Terminal = require('toggleterm.terminal').Terminal
+  local lazygit = Terminal:new { cmd = 'lazygit', direction = 'float', hidden = true }
 
   lazygit:toggle()
 end
 
 return {
   'akinsho/toggleterm.nvim',
-  version = "*",
+  version = '*',
   keys = {
-    { "<leader>at", "<cmd>ToggleTerm<cr>", desc = "Terminal" },
-    { "<leader>al", _lazygit_toggle,       desc = "LazyGit" }
+    { '<leader>at', '<cmd>ToggleTerm<cr>', desc = 'Terminal' },
+    { '<leader>al', _lazygit_toggle, desc = 'LazyGit' },
   },
   config = function()
-    local shell
-    if vim.fn.has("win32") then
-      shell = "pwsh.exe"
+    if vim.fn.has 'win32' ~= 0 then
+      vim.o.shell = 'pwsh.exe'
     end
 
-    require("toggleterm").setup({
-      shell = shell
-    })
-  end
+    require('toggleterm').setup()
+  end,
 }
-
